0c2975566fc47c24d1ddc124fc2dac6b3dcd5bdc
atiquehossain <atique.hossain@northsouth.edu>
2021-03-18 22:43:28 -0700

    Adapter_error_solve

---
+++ app/src/main/java/com/example/whatapps_layout_recycleview/Adapter.java
@@ -0,0 +1,38 @@
+package com.example.whatapps_layout_recycleview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+
+    private List<ModelClass> userList;
+
+
+    public Adapter(List<ModelClass> userList) {
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+    }
+}
