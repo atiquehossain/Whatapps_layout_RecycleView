6063938c21bc3364d90cc96a741e7f0f3f84bb75
atiquehossain <atique.hossain@northsouth.edu>
2021-03-18 22:26:44 -0700

    beforeAdapter

--- app/src/main/java/com/example/whatapps_layout_recycleview/ModelClass.java
+++ app/src/main/java/com/example/whatapps_layout_recycleview/ModelClass.java
@@ -1,4 +1,37 @@
 package com.example.whatapps_layout_recycleview;
 
 public class ModelClass {
+    private int imageview;
+    private String textviewOne;
+    private String textviewtwo;
+    private String textthree;
+    private String Devider;
+
+    public ModelClass(int imageview, String textviewOne, String textviewtwo, String textthree, String devider) {
+        this.imageview = imageview;
+        this.textviewOne = textviewOne;
+        this.textviewtwo = textviewtwo;
+        this.textthree = textthree;
+        Devider = devider;
+    }
+
+    public int getImageview() {
+        return imageview;
+    }
+
+    public String getTextviewOne() {
+        return textviewOne;
+    }
+
+    public String getTextviewtwo() {
+        return textviewtwo;
+    }
+
+    public String getTextthree() {
+        return textthree;
+    }
+
+    public String getDevider() {
+        return Devider;
+    }
 }
